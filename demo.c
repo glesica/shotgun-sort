aa058e8664db578f15b16e971b41ea999b1aa5d2
George Lesica <glesica@gmail.com>
2012-04-08 10:11:39 -0500

    Cleaned up demo program and added more examples.

--- demo.c
+++ demo.c
@@ -7,22 +7,43 @@
 #include <stdio.h>
 #include "sgsort.h"
 
-void print_array(void *base, size_t num, size_t size) {
+void test(
+    void *base,
+    size_t num,
+    size_t size,
+    int (*comparator) (const void *, const void *)
+) {
     int i;
+    
+    /* Assumes the test array is type int because I'm lazy. */
     int *b = (int *) base;
 
+    /* Output the original ordering. */
+    printf("Original: ");
     for (i=0; i<num; i++) {
         printf("%d ", b[i]);
     }
     printf("\n");
+
+    /* Release the hounds! */
+    sgsort(base, num, sizeof(int), comparator);
+
+    /* Output the (hopefully) sorted ordering. */
+    printf("Sorted:   ");
+    for (i=0; i<num; i++) {
+        printf("%d ", b[i]);
+    }
+    printf("\n\n");
 }
 
 int main() {
-    int i;
-    int test[] = {3,2,4,1,5};
+    int test1[] = {3,2,4,1,5};
+    test(test1, 5, sizeof(int), int_compare);
 
-    print_array(test, 5, sizeof(int));
-    sgsort(test, 5, sizeof(int), int_compare);
-    print_array(test, 5, sizeof(int));
+    int test2[] = {5,4,3,2,1};
+    test(test2, 5, sizeof(int), int_compare);
+
+    int test3[] = {5,9,1,8,2,7,4,6,3};
+    test(test3, 9, sizeof(int), int_compare);
 }
 
