278898d203f5aab997246302186629019343f09f
George Lesica <glesica@gmail.com>
2012-04-09 00:36:42 -0500

    Added a Python implementation, because I can.

---
+++ sgsort.py
@@ -0,0 +1,43 @@
+# sgsort.py
+# Author: George Lesica <glesica@gmail.com>
+# Description: A Python implementation of Shotgun Sort.
+
+import random
+
+
+def sgsorted(iterable, reverse=False):
+    """
+    Accepts an iterable `l` and returns a copy of 
+    it that has been sorted from "smallest" to 
+    "largest".
+
+    If `reverse` is True, descending order will be used.
+    """
+    def is_sorted(iterable):
+        a = iterable[0]
+        for i in iterable:
+            if i < a:
+                return False
+            a = i
+        return True
+
+    l = iterable[:] # Makes a copy of iterable
+
+    while (not is_sorted(l)):
+        random.shuffle(l)
+
+    if reverse:
+        l.reverse()
+
+    return l
+
+
+if __name__ == '__main__':
+    l = [3,2,4,1,5]
+    print l
+    print sgsorted(l)
+
+    l = [5,4,6,3,7,2,9,8,1]
+    print l
+    print sgsorted(l)
+
